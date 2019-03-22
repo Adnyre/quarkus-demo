ad639a3cabf7a89955210f82072d40bce1ab5db4
adnyre <adnyre@gmail.com>
2019-03-22 17:21:06 +0200

    added health check endpoint

--- quarkus-demo-orders/src/main/java/adnyre/quarkusdemo/orders/controller/OrderController.java
+++ quarkus-demo-orders/src/main/java/adnyre/quarkusdemo/orders/controller/OrderController.java
@@ -6,22 +6,30 @@ import adnyre.quarkusdemo.orders.service.OrderService;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/orders")
+@Path("/")
 public class OrderController {
 
     @Inject
     OrderService orderService;
 
     @GET
-    @Path("/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response healthCheck() {
+        return Response.ok("Ok", MediaType.TEXT_PLAIN).build();
+    }
+
+    @GET
+    @Path("/orders/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public OrderDto get(@PathParam("id") long id) {
         return orderService.getById(id);
     }
 
     @GET
+    @Path("/orders")
     @Produces(MediaType.APPLICATION_JSON)
     public List<OrderDto> get(@DefaultValue("false") @QueryParam("unprocessedOnly") boolean unprocessedOnly) {
         if (unprocessedOnly) {
@@ -32,13 +40,14 @@ public class OrderController {
     }
 
     @PUT
-    @Path("/{id}/process")
+    @Path("orders/{id}/process")
     @Produces(MediaType.APPLICATION_JSON)
     public OrderDto setProcessed(@PathParam("id") long id) {
         return orderService.setProcessed(id);
     }
 
     @POST
+    @Path("/orders")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public OrderDto save(OrderDto order) {
